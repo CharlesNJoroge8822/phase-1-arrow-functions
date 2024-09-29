1336e81e63bed87ea0a4d451ca30a61dfbe9906f
njorogecharles439@gmail.com <njorogecharles439@gmail.com.>
2024-09-29 15:57:42 +0300

    arrows

---
+++ index.js
@@ -0,0 +1,27 @@
+let a = 2000;
+let b = 100;
+
+
+function divide (a, b){
+    return a / b
+}
+console.log(divide(2000, 100));
+
+let square = (a) => {
+    return a * a
+
+};
+console.log(square(a));
+
+let add = (a, b) => {
+    return a + b
+};
+console.log(add(a, b));
+
+
+
+
+
+
+
+
